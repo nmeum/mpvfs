d7d09a8ec34d28e05063b88dbeadedd9b239ae26
Sören Tempel <soeren+git@soeren-tempel.net>
2021-01-15 22:18:55 +0100

    playvol: Add write support

--- playvol.go
+++ playvol.go
@@ -4,7 +4,6 @@ import (
 	"github.com/nmeum/mpvfs/mpv"
 	"github.com/nmeum/mpvfs/playlistfs"
 
-	"errors"
 	"io"
 	"strings"
 )
@@ -27,5 +26,17 @@ func (c playvol) Read(off int64, p []byte) (int, error) {
 }
 
 func (c playvol) Write(off int64, p []byte) (int, error) {
-	return 0, errors.New("not implemented")
+	vol, err := playlistfs.VolCmd(p)
+	if err != nil {
+		return 0, err
+	}
+
+	// TODO: handle channels
+	level := float64(vol.Levels[0])
+	err = c.mpv.SetProperty("volume", level)
+	if err != nil {
+		return 0, err
+	}
+
+	return len(p), nil
 }
