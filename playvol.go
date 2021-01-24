732d81f5ec28239b9492c6168ffcccba6c030f90
Sören Tempel <soeren+git@soeren-tempel.net>
2021-01-24 03:28:39 +0100

    playvol: implement blocking read

--- playvol.go
+++ playvol.go
@@ -5,29 +5,64 @@ import (
 	"github.com/nmeum/mpvfs/mpv"
 	"github.com/nmeum/mpvfs/playlistfs"
 
+	"errors"
 	"io"
 	"strings"
 )
 
 type playvol struct {
+	// Absolute offset at which (and beyond which) EOF will be returned.
+	// This member must be initalized with -1.
+	eofAt int64
+
+	// Absolute base offset used to calculate a relative offset for
+	// the current string reader.
+	baseOff int64
+
+	// Current string reader on which the read function operates.
+	reader *strings.Reader
+
 	state *playerState
 	mpv   *mpv.Client
 }
 
 func newVol() (fileserver.File, error) {
-	return &playvol{state, mpvClient}, nil
+	return &playvol{eofAt: -1, state: state, mpv: mpvClient}, nil
+}
+
+func (c *playvol) newReader(volume uint) *strings.Reader {
+	vol := playlistfs.Volume{[]uint{volume}}
+	return strings.NewReader(vol.String() + "\n")
 }
 
 func (c *playvol) Read(off int64, p []byte) (int, error) {
-	vol := playlistfs.Volume{[]uint{c.state.Volume()}}
-	reader := strings.NewReader(vol.String() + "\n")
+	if c.reader == nil {
+		c.reader = c.newReader(c.state.Volume())
+	} else if c.eofAt > 0 && off >= c.eofAt {
+		// We are reading beyond EOF for the second time.
+		// Block until new data is available and return it.
+		c.baseOff += c.reader.Size()
+		c.reader = c.newReader(c.state.WaitVolume())
+	} else if off < c.baseOff {
+		return 0, errors.New("invalid seek")
+	}
 
-	_, err := reader.Seek(off, io.SeekStart)
+	// Calculate offset relative to current reader
+	relOff := off - c.baseOff
+
+	_, err := c.reader.Seek(relOff, io.SeekStart)
 	if err != nil {
+		c.eofAt = off
 		return 0, io.EOF
 	}
 
-	return reader.Read(p)
+	n, err := c.reader.Read(p)
+	if err == io.EOF || (err != nil && n == 0) {
+		c.eofAt = off + int64(n)
+		return 0, io.EOF
+	}
+
+	return n, nil
 }
 
 func (c *playvol) Write(off int64, p []byte) (int, error) {
